a526cb24e1bf2e131e30c91607b9084baf745092
Vu Pham <vu@mellanox.com>
2015-12-11 22:02:38 +0200

    scripts: dos2unix re-format script files
    Signed-off-by: Vu Pham <vu@melalnox.com>

--- custom_settings.py
+++ custom_settings.py
@@ -6,9 +6,7 @@ RUN_TYPE = ['bvt', 'bst', 'fvt_inktank', 'fvt_storm']
 
 DEFAULT_RUN_LIST = {
 'bvt' : 'runlist/bvt_runlist',
-'bst' : 'runlist/bst_runlist',
-'fvt_inktank': 'runlist/fvt_inktank_runlist',
-'fvt_storm' : 'runlist/fvt_storm_runlist'}
+'bst' : 'runlist/bst_runlist'}
 
 #for emails not required to send mail
 #either comment it using '#' or remove them
@@ -22,6 +20,4 @@ SMTP_SERVER = ''
 
 LOG_PATH_LOCAL = {
 'bvt' : '/tmp/teuthology/logs/bvt/',
-'bst' : '/tmp/teuthology/logs/bst/',
-'fvt_inktank' : '/tmp/teuthology/logs/fvt_inktank/',
-'fvt_storm' : '/tmp/teuthology/logs/fvt_storm/'}
+'bst' : '/tmp/teuthology/logs/bst/'}
